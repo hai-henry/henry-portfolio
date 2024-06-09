81b1904cd4e78523bd83c11abb1ef597f867cba3
hai-henry <hvnryho@gmail.com>
2024-06-09 10:43:02 -0500

    Progress bar fixed but static

--- src/components/preloader/Preloader.tsx
+++ src/components/preloader/Preloader.tsx
@@ -1,65 +1,79 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 import './preloader.css';
 
-const Preloader = () => {
-    const load = gsap.timeline({ paused: true });
-    load.to('#percent,#bar', { duration: 0.2, opacity: 0, zIndex: -1 });
-    load.to('.progress', {
-        duration: 0.8,
-        width: '0%',
-    });
-    load.from(
-        '.main__page__content',
-        {
-            duration: 0.8,
-            opacity: 0,
-            ease: 'power4.out',
-        },
-        '-=.5'
-    );
-    load.from(
-        '.main__page__content h1',
-        {
-            duration: 0.5,
-            y: 50,
-            skewY: 10,
-            opacity: 0,
-        },
-        '-=1'
-    );
-
-    const [width, setWidth] = useState(0);
-    const [id, setId] = useState(0);
-
-    function frame() {
-        if (width >= 100) {
-            clearInterval(id);
-            load.play();
-        } else {
-            setWidth((prev) => prev + 1);
-            document.getElementById('bar__confirm')!.style.width = `${width}%`;
-            document.getElementById('percent')!.textContent = `${width}%`;
-        }
-    }
-
-    function loading() {
-        setId(setInterval(frame, 25));
-    }
+const Preloader: React.FC = () => {
+    const [width, setWidth] = useState<number>(1);
+    const intervalId = useRef<number | null>(null);
+    const load = useRef<gsap.core.Timeline>();
 
     useEffect(() => {
-        loading();
-    }),
-        [];
+        load.current = gsap.timeline({ paused: true });
+        load.current.to('#percent,#bar', {
+            duration: 0.2,
+            opacity: 0,
+            zIndex: -1,
+        });
+        load.current.to('.progress', {
+            duration: 0.8,
+            width: '0%',
+        });
+        load.current.from(
+            '.main__page__content',
+            {
+                duration: 0.8,
+                opacity: 0,
+                ease: 'power4.out',
+            },
+            '-=.5'
+        );
+        load.current.from(
+            '.main__page__content h1',
+            {
+                duration: 0.5,
+                y: 50,
+                skewY: 10,
+                opacity: 0,
+            },
+            '-=1'
+        );
+    }, []);
+
+    useEffect(() => {
+        function frame() {
+            setWidth((prev) => {
+                if (prev < 100) {
+                    return prev + 1;
+                } else {
+                    if (intervalId.current) {
+                        clearInterval(intervalId.current);
+                    }
+                    load.current?.play();
+                    return prev;
+                }
+            });
+        }
+
+        intervalId.current = window.setInterval(frame, 25);
+
+        return () => {
+            if (intervalId.current) {
+                clearInterval(intervalId.current);
+            }
+        };
+    }, []);
 
     return (
         <>
             <div className="loader">
                 <div className="progress">
                     <div id="bar">
-                        <div id="bar__confirm"></div>
+                        <div
+                            id="bar__confirm"
+                            style={{ width: `${width}%` }}
+                        ></div>
                     </div>
-                    <div id="percent">0%</div>
+                    <div id="percent">{width}%</div>
                 </div>
             </div>
             <div className="main__page">
