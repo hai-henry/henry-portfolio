b111298e5e9572950b07a1e17d0ce7f1cec74b4b
hai-henry <hvnryho@gmail.com>
2024-08-30 20:59:38 -0500

    Footer updated for tablet

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,22 +5,54 @@ const Footer = () => {
         <>
             <footer className="w-screen h-3/4 flex flex-col gap-y-5 justify-end">
                 <div
-                    className="w-auto h-auto flex flex-row justify-between px-5 pt-8
-                    font-montrealMedium text-xl uppercase"
+                    className="w-auto h-auto flex flex-row justify-between 
+                    px-5 lg:px-28
+                    py-8
+                    font-montrealMedium 
+                    text-xl lg:text-3xl
+                    uppercase"
                 >
                     <div className="w-auto h-auto flex flex-col items-start ">
-                        <button className="uppercase">Email</button>
-                        <button className="uppercase">Github</button>
-                        <button className="uppercase">Linkedin</button>
+                        <button className="uppercase">
+                            <a
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                href="mailto:hvnryho@gmail.com"
+                            >
+                                Email
+                            </a>
+                        </button>
+                        <button className="uppercase">
+                            <a
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                href="https://github.com/hai-henry"
+                            >
+                                Github
+                            </a>
+                        </button>
+                        <button className="uppercase">
+                            <a
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                href="https://www.linkedin.com/in/henryho01/"
+                            >
+                                Linkedin
+                            </a>
+                        </button>
                     </div>
                     <div>
                         <button className="uppercase">Back to top</button>
                     </div>
                 </div>
-                <h1 className="text-center text-7xl font-montrealBold uppercase">
+                <h1
+                    className="text-center 
+                text-7xl lg:text-9xl
+                font-montrealBold uppercase"
+                >
                     Get in touch
                 </h1>
-                <p className="text-center text-xl uppercase px-5 pb-5">
+                <p className="text-center text-xl uppercase px-5 py-5">
                     Designed and Developed by Henry Ho
                 </p>
             </footer>
