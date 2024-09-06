e7ff2ef54d462fa0ca2939c533a449b5b75a5d06
hai-henry <hvnryho@gmail.com>
2024-09-06 13:58:24 -0500

    Start scrolltotop component

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -1,1 +1,14 @@
+import React from 'react';
 // TODO: Create functionality for scrolltotop
+
+const ScrollToTop = (props) => {
+    return (
+        <>
+            <div>
+                <button onClick={props.scrollUp}>Back to top</button>
+            </div>
+        </>
+    );
+};
+
+export default ScrollToTop;
