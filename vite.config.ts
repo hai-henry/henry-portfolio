9948313754beef22c60164bde1ca97e7fd0b4ac8
hai-henry <hvnryho@gmail.com>
2024-09-10 19:35:51 -0500

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: '', // Ensure this matches your GitHub repository path
+    plugins: [react()],
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+    },
+});
