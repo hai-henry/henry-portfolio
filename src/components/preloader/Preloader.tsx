c9b100850f23cf72f0ca133216d3d95a81c476c5
hai-henry <hvnryho@gmail.com>
2024-09-15 08:06:25 -0500

    Update build

--- src/components/preloader/Preloader.tsx
+++ src/components/preloader/Preloader.tsx
@@ -6,7 +6,7 @@ Refer to LettersFadeIn component to make changes to the text that fades in.
 
 import React, { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
-import LettersFadeIn from '../LettersFadeIn/Lettersfadein';
+import LettersFadeIn from '../LettersFadeIn/Lettersfadein.tsx';
 
 const Preloader: React.FC = () => {
     const [width, setWidth] = useState<number>(1); // Loading bar & percentage value
